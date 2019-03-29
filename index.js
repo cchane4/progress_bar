5826c3363dc2967b3e1906c6f03dbb9b905f0cce
Chad Chaney <cchane4@gmail.com>
2019-03-28 20:01:58 -0500

    still trying to write in es6

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//function move defines elem, width and id(which is set to a setinterval function
+//that increments by 10); function frame states that if the width is greater than or 
+//equal to 100, otherwise increase the width and sets the bar element's width styling to 
+//width plus the percentage 
+
 /*function move(){ 
     var elem = document.getElementById("bar"); 
     var width = 1; 
@@ -13,8 +18,38 @@
         }
     }   
 }*/
- 
-let elem = document.getElementById("bar"); 
-let width = 1; 
-let id = setInterval(frame, 10); 
 
+/* if(this.width>=100) { 
+        clearInterval(id); 
+    }
+    else{ 
+        this.width++; 
+       elem.style.width = width+'%'; 
+  }
+
+} */ 
+
+setInterval(() => { 
+    frame; 
+    },10); 
+
+ 
+
+document.addEventListener("DOMContentLoaded",() => {
+    let elem = document.getElementById("bar");
+    let btn_press = document.getElementById("press");
+    //let id  = setInterval ((10); 
+    let width = 1;
+     
+  });
+
+
+ let frame = () => { 
+    if(this.width>=100) { 
+        clearInterval(id); 
+    }
+    else{ 
+        this.width++; 
+       elem.style.width = width+'%'; 
+    }
+}
